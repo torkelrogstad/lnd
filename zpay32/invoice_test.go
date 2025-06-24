c7c38c1a13164f1eec31fcb752b251c73a93ab15
MPins <mpinsdorf@gmail.com>
2025-06-24 16:29:21 -0300

    zpay32: test case for parsing non UTF-8 description
    (cherry picked from commit b8acca2c892d1961e65a6b58dcb925c41cc5c3d0)

--- zpay32/invoice_test.go
+++ zpay32/invoice_test.go
@@ -49,6 +49,7 @@ var (
 		0x11, 0x11, 0x11, 0x11, 0x11, 0x11, 0x11, 0x11,
 	}
 
+	testNonUTF8Str      = "1 cup coffee\xff\xfe\xfd"
 	testEmptyString     = ""
 	testCupOfCoffee     = "1 cup coffee"
 	testCoffeeBeans     = "coffee beans"
